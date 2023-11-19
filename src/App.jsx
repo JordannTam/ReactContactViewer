0f3d57ac859d3b274ecf86e7a66aa825bf0b96ce
Jordan Tam <z5254001@ad.unsw.edu.au>
2023-11-20 04:27:36 +1100

    [EDIT] README.md

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,13 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen'
-import ContactScreen from './screens/ContactScreen';
 import { Typography } from '@mui/material';
 
 function App() {
   return (
     <BrowserRouter>
-        {/* <Navbar /> */}
         <Routes>
-          <Route path='/' element={<HomeScreen />}>
-            <Route path=':id' element={<ContactScreen />} />
-          </Route>
+          <Route path='/' element={<HomeScreen />}></Route>
           <Route path="*" element={
             <Typography variant="h3" component="h3">
               <h3> 404 Page not found</h3>
